69850f35b0d8ee02bbc33865af92b2441921e3e5
George Lin <140451577+georgel735@users.noreply.github.com>
2026-06-24 18:25:13 -0400

    feat: finished aoide page!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
-import { defineConfig } from 'astro/config'
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 export default defineConfig({
   integrations: [tailwind(), react()],
-})
+});
